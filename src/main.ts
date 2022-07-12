e4018536fb716cf188eb13a4b6cbf707e67b3e91
Lenochka <lvutina.en@gmail.com>
2022-07-12 21:17:33 +0100

    init: add yaml to swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule } from '@nestjs/swagger';
+import { readFile } from 'fs/promises';
+import { parse } from 'yaml';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const DOC_API = await readFile(
+    path.join(__dirname, '../', 'doc', 'api.yaml'),
+    'utf-8',
+  );
+  const document = parse(DOC_API);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(4000);
 }
+
 bootstrap();
