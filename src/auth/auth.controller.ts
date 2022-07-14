12c727f067524a3b8299815bf568ddb4ecb68657
Lenochka <lvutina.en@gmail.com>
2022-07-14 12:34:09 +0100

    init: add prisma and docker

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,22 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
+    console.log({ dto });
     return this.authService.signup();
   }
+  //PasrseIntPipe validates that password is a number
   @Post('signin')
-  signin() {
+  signin(
+    @Body('email') email: string,
+    @Body('password', ParseIntPipe) password: string,
+  ) {
+    console.log({ email, password });
     return this.authService.signin();
   }
 }
